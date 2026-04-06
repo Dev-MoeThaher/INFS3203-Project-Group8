87ebd6327208f44d027449def5a89b7deccd564f
Mohammad Thaher <mohammedthaher12@gmail.com>
2026-04-07 00:48:36 +0300

    feat: add Trip CRUD API endpoints (#10)

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const healthRoutes = require("./routes/healthRoutes");
+const tripRoutes = require("./routes/tripRoutes");
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/health", healthRoutes);
+app.use("/api/trips", tripRoutes);
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
